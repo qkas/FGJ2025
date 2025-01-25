fbd54b8a707acf7a3e8fff0d6a671671e382eddf
Kasperi Jaakkola <kassuvaa@gmail.com>
2025-01-25 04:02:09 +0200

    Added game start with animation

--- entities/boat/boat.gd
+++ entities/boat/boat.gd
@@ -1,31 +1,13 @@
 extends CharacterBody3D
 
 @onready var camera_holder: Node3D = $CameraHolder
+@onready var animation_player: AnimationPlayer = $CameraHolder/AnimationPlayer
 
-const max_zoom: float = 1
-const min_zoom: float = -1
-const drag_force: float = 5
+const MAX_ZOOM: float = 1
+const MIN_ZOOM: float = -1
+
 const SPEED = 5.0
-var zoomed_in : bool = false;
 
-
-func _input(event: InputEvent) -> void:
-	if event.is_pressed():
-		if event is InputEventMouseButton:
-			if event.button_index == MOUSE_BUTTON_WHEEL_UP && camera_holder.position.y < max_zoom:
-				camera_holder.position.y += 0.1
-			elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN  && camera_holder.position.y > min_zoom:
-				camera_holder.position.y -= 0.1
-				
-	if Input.is_key_pressed(KEY_C):
-		#plays this in reverse
-		if zoomed_in:
-			$CameraHolder/AnimationPlayer.play("zoom_in",-1,-1, true)
-			zoomed_in = false
-		else:
-			$CameraHolder/AnimationPlayer.play("zoom_in")
-			zoomed_in = true
-				
 func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (transform.basis * Vector3(0, 0, input_dir.y)).normalized()
@@ -39,3 +21,14 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED*0.01)
 
 	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event.is_pressed():
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP and camera_holder.position.y < MAX_ZOOM:
+				camera_holder.position.y += 0.1
+			elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and camera_holder.position.y > MIN_ZOOM:
+				camera_holder.position.y -= 0.1
+
+func camera_zoom_in():
+	animation_player.play("initialize")
--- scenes/sea/game.gd
+++ scenes/sea/game.gd
@@ -1,11 +1,8 @@
 extends Node3D
 
+@onready var fisher: Node3D = $Boat/Fisher
+@onready var boat: CharacterBody3D = $Boat
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func start_game():
+	boat.camera_zoom_in()
+	$CanvasLayer/MainMenu.hide()
