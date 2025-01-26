39d1d8e5191c936f0a753c9306686491e0df60a9
Niroasd <nico.roininen@edu.turkuamk.fi>
2025-01-26 10:30:29 +0200

    Bubble changes

--- ui/task_entry.gd
+++ ui/task_entry.gd
@@ -4,4 +4,4 @@ func set_label(text: String) -> void:
 	$TextMargin/Label.text = text
 
 func check_task() -> void:
-	$TextMargin/ProgressBar/AnimationPlayer.play("progress_bar")
+	$TextMargin/ProgressBar/AnimationPlayer.play("check_done")
