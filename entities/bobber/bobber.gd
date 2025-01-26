ee081c439b96e78c0d28405f3bd75a5b7335c2e1
Kasperi Jaakkola <kassuvaa@gmail.com>
2025-01-26 03:14:20 +0200

    Added fishing mechanics and lowered resolution

--- entities/bobber/bobber.gd
+++ entities/bobber/bobber.gd
@@ -1,16 +1,21 @@
 extends RigidBody3D
 
+signal on_bobber_landed
+
 @export var buoyancy_power: float = 0
 
 @onready var water_manager: WaterManager = $/root/Main/WaterManager
+
+var is_flying: bool = false
 
 # An incredibly simple showcase of the buoyancy
 func _physics_process(delta: float) -> void:
 	var _water_height = water_manager.calc_water_height(global_position)
 	
-	if global_position.y > _water_height:
-		gravity_scale = 1
-	else:
+	if global_position.y <= _water_height:
+		if is_flying:
+			is_flying = false
+			on_bobber_landed.emit()
 		global_position.y = _water_height + 0.1
 		linear_velocity.x = 0
 		linear_velocity.z = 0
--- entities/fisher/fisher.gd
+++ entities/fisher/fisher.gd
@@ -3,29 +3,46 @@ extends Node3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var bobber: RigidBody3D = $Bobber
 
-var is_fishing: bool = false
-var cast_strength : float = 5.00
+var cast_strength : float = 2.2
+
+var timer_range_min: float = 3
+var timer_range_max: float = 12
 
 func _input(_event: InputEvent) -> void:
+	# if not $/root/Main.is_game_active:
+	#	return
 	if Input.is_action_just_pressed("fish"):
 		var curr_anim = animation_player.current_animation
-		if curr_anim == 'fishing_idle' and not is_fishing:
+		if curr_anim == 'fishing_idle':
 			animation_player.play("fishing_cast")
-			bobber.freeze = false
-			var global_direction = global_transform.basis * Vector3(0, 2, -1)
-			bobber.apply_impulse(global_direction * cast_strength)
+			$CastingTimer.start() # after delay calls throw hook
+		else: # not currently fishing
+			animation_player.play("fishing_idle")
 
-func start_minigame(difficulity: float) -> void:
-	pass
+func cancel_fishing() -> void:
+	animation_player.stop()
+	animation_player.play('fishing_idle')
 
-func get_fish() -> void:
+func throw_hook() -> void:
+	bobber.freeze = false
+	bobber.is_flying = true
+	var global_direction = global_transform.basis * Vector3(0, 2, -1)
+	bobber.apply_impulse(global_direction * cast_strength)
+
+func start_timer() -> void: # called on bobber landed 
+	$FishingTimer.start(randf_range(timer_range_min, timer_range_max))
+
+func get_random_fish() -> void:
 	pass
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	animation_player.play('fishing_idle')
 	if anim_name == 'fishing_cast':
-		is_fishing = true
-		bobber.freeze = false
-		bobber.apply_impulse(Vector3(0, 2, -1) * cast_strength)
+		animation_player
 	if anim_name == 'fishing_reel':
-		get_fish()
+		get_random_fish()
+		bobber.freeze = true
+		animation_player.stop()
+		animation_player.play('fishing_idle')
+
+func start_reeling() -> void: # on timer timeout
+	animation_player.play('fishing_reel')
